55e47a0d95d134b44ab243798dee2c0efaa7b3a8
Benjamin Leonhardt <bjle71347@edu.ucl.dk>
2025-08-14 13:07:07 +0200

    Tilføjet mappestruktur og RelayCommand class

--- TheMovies/obj/Debug/net8.0-windows/TheMovies.AssemblyInfo.cs
+++ TheMovies/obj/Debug/net8.0-windows/TheMovies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd8667bcb945c7a7ab1a312a2852223cd8ac0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
