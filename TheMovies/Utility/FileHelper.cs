daeb10fb9bd1669dcd676d029b905c382bb81789
Wuway <contactdieter@gmail.com>
2025-08-24 07:52:02 +0200

    tilføjet tjek om fil eksisterer -> ellers lav en operettet filehelper class i utility mappen med ensurefileexists()

---
+++ TheMovies/Utility/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TheMovies.Utility
+{
+    public static class FileHelper
+    {
+        public static void EnsureFileExists(string filePath)
+        {
+            if (!File.Exists(filePath)) 
+            {
+                using (File.Create(filePath)) ;
+                // brug using i stedet for File.Create, fordi using automatisk bruge Dispose() korrekt
+                //File.Create(filePath).Dispose();
+                // can også bruge File.Create(filePath).Close(); men anbefales at bruge Dispose(): "har ikke bruge mere for dette, så ryd op"
+
+            }
+        }
+    }
+}
